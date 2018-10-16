26a3cf700fb31ebad1d8fdab83308ed580d5190c
Zubair Amjad <amjadz@uw.edu>
2018-10-15 22:04:36 -0700

    Initial Commit

---
+++ simple-calc-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-calc-ios
+//
+//  Created by Zubair Amjad on 10/15/18.
+//  Copyright © 2018 Zubair Amjad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
